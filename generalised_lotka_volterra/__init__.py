75135867b891f882aa7a65dc2596f26ab3cb15b4
Lyle Poley <109674842+LylePoley@users.noreply.github.com>
2025-02-20 20:56:08 +0000

    Delete generalised_lotka_volterra directory

--- generalised_lotka_volterra/__init__.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
-
-from generalised_lotka_volterra.finely_structured_random_matrix import fs_statistics, fsrm_instance, ust_to_mu_sigma_gamma, mu_sigma_gamma_to_ust, elliptical_instance
-from generalised_lotka_volterra.simulation import *
-from generalised_lotka_volterra.theory import *
-from generalised_lotka_volterra.w_functions import w0, w1, w2
