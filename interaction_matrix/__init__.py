788cfb2219a3328cc8887525bda62c0dff939d69
Lyle Poley <109674842+LylePoley@users.noreply.github.com>
2025-02-20 20:55:20 +0000

    Deleted unecessary files and directories

--- interaction_matrix/__init__.py
+++
@@ -1,5 +0,0 @@
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
-
-from interaction_matrix.finely_structured_random_matrix import \
-fs_statistics, fsrm_instance, ust_to_mu_sigma_gamma, mu_sigma_gamma_to_ust, elliptical_instance
