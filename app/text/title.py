e48415f6288795a0f23a83832690e21f2bf96fe7
Lyle Poley <poleylyle@gmail.com>
2025-07-15 14:01:18 +0100

    Cleaned up introductory paragraph

--- app/text/title.py
+++ app/text/title.py
@@ -7,14 +7,16 @@ text.append(
 text.append(
         r"""
         This is an interactive tool to explore the dynamics of disordered
-        dynamical systems with pairwise interactions. It includes The
-        $N \times N$ matrix $\underline{\underline{\alpha}}$ encodes the
-        interactions between the different agents in the model.
-        $\underline{\underline{\alpha}}$ is a random matrix whose elements
-        are drawn identically and independently from a Gaussian distribution
-        with mean $\frac{\mu}{N}$ and variance $\frac{\sigma^2}{N}$. The
-        'Re-draw noise' button re-generates these random interaction
-        coefficients."""
+        dynamical systems with pairwise interactions. For each of the dynamical systems available, there are $N$ agents,
+        and $\underline{\underline{\alpha}}$ is a matrix containing their pairwise interaction strengths ($\alpha_{ij}$
+        encodes the effect of agent $j$ on agent $i$). Each coefficient of the interaction matrix $\underline{\underline{\alpha}}$ is
+        drawn independently at random from a Gaussian distribution with mean $\frac{\mu}{N}$ and variance $\frac{\sigma^2}{N}$.
+        The options in the sidebar allow you to modify the mean and variance of the interaction statistics ($\mu$ and $\sigma$),
+        the number of agents ($N$), as well as the maximum time to integrate the dynamics up to $(T)$. The 'Re-draw noise'
+        button re-generates $\underline{\underline{\alpha}}$ with the current parameters, the 'Re-draw initial condition' button
+        re-generates the initial conditions for the agents, which are drawn from a uniform distribution in the interval $[0, 1]$.
+        No integration is performed until the 'Integrate dynamics' button is pressed.
+        """
 )
 text.append(
         f"""
